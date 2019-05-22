a5e54dba71ed0bddb19d528051a2ef12ac1a050f
sonali-Packt <sonalis@packtpub.com>
2019-05-22 16:33:41 +0530

    Code Uploaded

---
+++ Section 1/Recursion/IndirectRecursion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace IndirectRecursion
+{
+    public partial class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckNumberFive();
+        }
+    }
+
+    public partial class Program
+    {
+        private static void CheckNumberFive()
+        {
+            Console.WriteLine(
+                "Is 5 even number? {0}",
+                IsEven(5));
+        }
+    }
+
+    public partial class Program
+    {
+        private static bool IsOdd(int targetNumber)
+        {
+            if (targetNumber == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return IsEven(targetNumber - 1);
+            }
+        }
+
+        private static bool IsEven(int targetNumber)
+        {
+            if (targetNumber == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return IsOdd(targetNumber - 1);
+            }
+        }
+    }
+}
